3596b6dba40872ae50ff3a2129b0140f68b6b25a
D-pixel-crime <dpk4383@gmail.com>
2024-05-17 19:40:27 +0530

    initial file structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <article className="h-screen w-screen bg-black">
+      <Navbar />
+    </article>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+const Navbar = () => {
+  return (
+    <header className="fixed z-50 top-0 left-0 w-full py-3 px-8 bg-neutral-800 h-fit text-white">
+      <nav>
+        <ul className="w-full flex justify-between items-center">
+          <li className="flex-center text-2xl">
+            <img
+              src="/motion art/navbar-leaf.png"
+              alt="navbar leaf symbol"
+              height={37}
+              width={37}
+              className="inline-block flex-center"
+            />
+            <span className="font-semibold flex-center">envato</span>
+            <span className="text-[yellowgreen] font-extralight flex-center">
+              market
+            </span>
+          </li>
+          <li className="flex-center">
+            <button className="bg-[yellowgreen] flex-center shadow-md shadow-black rounded-lg text-black font-semibold hover:bg-transparent hover:text-[yellowgreen] border border-[yellowgreen] px-2 py-1 tracking-tighter">
+              Buy now
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Navbar;
